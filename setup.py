7ef7970551771711e1d6d3809a73816ce366022e
juanguillermo3 <135280741+juanguillermo3@users.noreply.github.com>
2023-07-26 15:50:18 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name="import_master",
     version="0.0.1",
-    packages=find_packages(),
-    install_requires=[],  # List your dependencies here
+    packages=find_packages(),  # It should discover your "import_master" package
     author="Your Name",
     author_email="your.email@example.com",
     description="A Python package for ...",
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
     url="https://github.com/juanguillermo3/data_enhancement_proposals"
 )
